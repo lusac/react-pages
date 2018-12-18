87260c1bd906ef2f37949f5226b0f2ae19e2cfd6
Lucas Chuairi Cruz <lusacrj@gmail.com>
2018-12-18 10:31:44 -0200

    Create a search input inside editor popin

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './index.scss'
-import { components } from '../../data/components';
+import Search from '@material-ui/icons/Search'
+import { components } from '../../data/components'
 
-const PopinItem = ({ item, selected, clickHandler} = {}) => {
+const SearchInput = ({ onChangeHandler } = {}) => {
+  return (
+    <span className="editor__popin__search">
+      <label htmlFor="popin-search"><Search /></label>
+      <input id="popin-search" onChange={onChangeHandler} />
+    </span>
+  )
+}
+
+const PopinItem = ({ item, selected, onClickHandler} = {}) => {
   const classNames = 'editor__popin__item ' + (selected ? ' editor__popin__item--selected' : '');
 
   return (
-    <div className={classNames} onClick={clickHandler.bind(this, item.id)}>
+    <div className={classNames} onClick={onClickHandler.bind(this, item.id)}>
       <figure>
         <img src={item.thumbnail} />
         <figcaption>{item.name}</figcaption>
@@ -17,8 +27,13 @@ const PopinItem = ({ item, selected, clickHandler} = {}) => {
 
 export default class Editor extends React.Component {
   state = {
+    searchTerm: '',
     showPopin: true,
     itemSelected: null
+  }
+
+  searchOnChange(e) {
+    this.setState({ searchTerm: e.target.value })
   }
 
   openPopin() {
@@ -39,6 +54,12 @@ export default class Editor extends React.Component {
     })
   }
 
+  getComponents() {
+    if (this.state.searchTerm.length === 0)
+      return components
+    return components.filter((c) => c.name.toLocaleLowerCase().indexOf(this.state.searchTerm.toLocaleLowerCase()) >= 0)
+  }
+
   render() {
     return (
       <div>
@@ -50,15 +71,16 @@ export default class Editor extends React.Component {
           <div className="editor__popin">
             <div className="editor__popin__content">
               <div className="editor__popin__content__header">
-                <h2>Choose a widget:</h2>
+                <h2>Choose a widget</h2>
+                <SearchInput onChangeHandler={this.searchOnChange.bind(this)} />
               </div>
               <div className="editor__popin__content__section">
-                {components.map((c, i) => (
+                {this.getComponents().map((c, i) => (
                   <PopinItem
                     key={i}
                     item={c}
                     selected={c.id === this.state.itemSelected}
-                    clickHandler={this.itemOnClick.bind(this)}
+                    onClickHandler={this.itemOnClick.bind(this)}
                   />))}
               </div>
               <div className="editor__popin__content__footer">
